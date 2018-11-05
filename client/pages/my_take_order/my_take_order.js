3f356509ab24b513ef696211a9aa7e884c3e4115
picksh <490417155@qq.com>
2018-11-06 00:24:55 +0800

    加入我的接单和我的下单
    并没有写完

---
+++ client/pages/my_take_order/my_take_order.js
@@ -0,0 +1,70 @@
+Page({
+  data: {
+    tabList: ['进行中', '已完成'],
+    current: 0,//当前选中的Tab项
+  },
+
+  /**
+   * Tab的点击切换事件
+   */
+  tabItemClick: function (e) {
+    this.setData({
+      current: e.currentTarget.dataset.pos
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
