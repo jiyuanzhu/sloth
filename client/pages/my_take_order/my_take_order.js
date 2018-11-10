ea49dda897490b6e2bd26f8e96332ad90df2771b
picksh <490417155@qq.com>
2018-11-10 10:26:00 +0800

    我的下单/接单
    还没有写逻辑

--- client/pages/my_take_order/my_take_order.js
+++ client/pages/my_take_order/my_take_order.js
@@ -1,7 +1,76 @@
+var app = getApp();
+var config = require('../../config');
+
 Page({
   data: {
     tabList: ['进行中', '已完成'],
     current: 0,//当前选中的Tab项
+
+    typeID: 0,
+    isLoading: true,
+    loadOver: false,
+    order: [],
+    districtList: [{
+      key: 1,
+      value: "C1"
+    }, {
+      key: 2,
+      value: "C2"
+    }, {
+      key: 3,
+      value: "C3"
+    }, {
+      key: 4,
+      value: "C4"
+    }, {
+      key: 5,
+      value: "C5"
+    }, {
+      key: 6,
+      value: "C6"
+    }, {
+      key: 7,
+      value: "C7"
+    }, {
+      key: 8,
+      value: "C8"
+    }, {
+      key: 9,
+      value: "C9"
+    }, {
+      key: 10,
+      value: "C10"
+    }, {
+      key: 11,
+      value: "C11"
+    }, {
+      key: 12,
+      value: "C12"
+    },{
+      key:13,
+      value:"全部"
+    }],
+    sortingList: [{
+      key: 1,
+      value: "智能排序"
+    }, {
+      key: 2,
+      value: "价格最低"
+    }, {
+      key: 3,
+      value: "价格最高"
+    }, {
+      key: 4,
+      value: "销量最高"
+    }],
+    chioceDistrict: false,
+    chioceSorting: false,
+    activeDistrictIndex: -1,
+    activeDistrictName: "楼栋",
+    scrollTop: 0,
+    scrollIntoView: 0,
+    activeSortingIndex: -1,
+    activeSortingName: "购买店铺"
   },
 
   /**
@@ -12,59 +81,139 @@ Page({
       current: e.currentTarget.dataset.pos
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
+
   onLoad: function (options) {
-
+    var that = this;
+    wx.request({
+      url: config.service.take_order_homeUrl,
+      method: "GET",
+      header: {
+        "content-type": "application/json"
+      },
+      success: function (res) {
+        that.setData({
+          order: res.data.data.data
+        });
+        // console.log(res.data)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
-
+    this.setData({
+      productList: [],
+      pageIndex: 1,
+      loadOver: false,
+      isLoading: true
+    })
+    //this.getProductList();
+    wx.stopPullDownRefresh()
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
+    if (!this.data.loadOver) {
+      this.setData({
+        pageIndex: this.data.pageIndex + 1,
+        isLoading: true,
+        loadOver: false
+      })
+      //this.getProductList();
+    }
+  },
+  //条件选择
+  choiceItem: function (e) {
+    switch (e.currentTarget.dataset.item) {
+      case "1":
+        if (this.data.chioceDistrict) {
+          this.setData({
+            
+            chioceDistrict: false,
+            chioceSorting: false,
+            chioceFilter: false,
+          });
+        } else {
+          this.setData({
+            
+            chioceDistrict: true,
+            chioceSorting: false,
+            chioceFilter: false,
+          });
+        }
+        break;
+      case "2":
+        if (this.data.chioceSorting) {
+          this.setData({
+            
+            chioceDistrict: false,
+            chioceSorting: false,
+            chioceFilter: false,
+          });
+        } else {
+          this.setData({
+            chioceDistrict: false,
+            chioceSorting: true,
+            chioceFilter: false,
+          });
+        }
+        break;
+    }
+  },
+  hideAllChioce: function () {
+    this.setData({
+     
+      chioceDistrict: false,
+      chioceSorting: false,
+      chioceFilter: false,
+    });
+  },
+  //区域位置
+  getDistrictList: function () {
+    var that = this;
+    wx.request({
+      url: app.globalData.hostUrl,
+      data: {
+        message: "20005",
+        location_id: that.data.locationID,
+        token: md5.hex_md5(app.globalData.token),
+        device_source: app.globalData.deviceSource
+      },
+      success: function (resRequest) {
+        if (resRequest.data.error_code == 0) {
+          that.setData({
+            districtList: resRequest.data.district_list
+          })
+        }
+      }
+    })
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
+  districtSorting: function (e) {
+    var index = e.currentTarget.dataset.index;
+    this.setData({
+    
+      chioceDistrict: false,
+      activeDistrictIndex: index,
+      activeDistrictName: this.data.districtList[index].value,
+      productList: [],
+      pageIndex: 1,
+      loadOver: false,
+      isLoading: true
+    })
+    //this.getProductList();
+  },
+  //综合排序
+  selectSorting: function (e) {
+    var index = e.currentTarget.dataset.index;
+    this.setData({
+     
+      chioceSorting: false,
+      activeSortingIndex: index,
+      activeSortingName: this.data.sortingList[index].value,
+      productList: [],
+      pageIndex: 1,
+      loadOver: false,
+      isLoading: true
+    })
+    //this.getProductList();
   }
+
 })
