a8d8fca69fe9140843e4e52e8876aee4d292a88a
Vergissmeinnichtz <33996593+Vergissmeinnichtz@users.noreply.github.com>
2018-11-20 19:26:07 +0800

    我的订单

--- server/controllers/my_order.js
+++ server/controllers/my_order.js
@@ -1,76 +1,69 @@
 const { mysql } = require('../qcloud')
 
 module.exports = async ctx => {
-  var user_id = ctx.request.query.user_id
-  var res = await mysql("foodOrder").where({ user_id })
-  //ctx.state.data=res
+  var open_id = ctx.request.query.user_id
+  var res = await mysql("foodOrder").where({ open_id})
   var num = res.length
-  var count = 0
   var str = "{\"data\":["
+  var count = 0
   for (var i = 0; i < num; i++) {
-    var food_order_id
-    if (res[i].food_order_id != undefined)
-      food_order_id = res[i].food_order_id
+    var order_id
+    if (res[i].order_id != undefined)
+      order_id = res[i].order_id
     else
       continue
-    var res1 = await mysql("foodOrderDetail").where({ food_order_id })
+    var res1 = await mysql("foodOrderDetail").where({ order_id })
     var order_total_item = res1.length
     var good_id
     if (res1.length != 0)
       good_id = res1[0].good_id
     else
       continue
-    var res4 = await mysql("catalog_good").where({ good_id })
+    var res4 = await mysql("foodMenu").where({ good_id })
     var order_good_name
     if (res4.length != 0)
       order_good_name = res4[0].good_name
     else
-      order_good_name="不知道吃什么"
+      continue
     var shop_id = good_id.substr(1, 1)
-    var res2 = await mysql("shop").where({ shop_id })
+    var res2 = await mysql("foodShop").where({ shop_id })
 
     var order_shop_address
     if (res2.length != 0)
       order_shop_address = res2[0].shop_intro
     else
-      order_shop_address="不知道哪里的店"
+      continue
     var order_deli_fee = res2[0].shipping_fee
 
     var order_good_num = res1[0].good_order_num
-    var res3 = await mysql("contactInfo").where({ user_id })
-    //ctx.state.data=res3
+    var res3 = await mysql("foodContactInfo").where({ open_id })
     var order_address
     if (res3.length != 0)
-      order_address = res3[0].address
+      order_address = res3[0].user_address
     else
-      order_address="不知道送到哪里"
+      continue
     var res5
-    var order_tiem
-    if (res[i].food_order_time != undefined){
-      res5 = res[i].food_order_time
-      order_time = res5.substr(5)
-    }
+    if (res[i].order_time != undefined)
+      res5 = res[i].order_time
     else
-      order_time="不知道什么时候送到"
-   
+      continue
+
+    order_time = res5.substr(5)
     var order_sum
     if (res[i].total_cost != undefined)
       order_sum = res[i].total_cost
     else
       continue
-    var res6 = await mysql("orderinfo").where({ food_order_id })
-    var order_state 
-    if( res6.length != 0)
-      order_state = res6[0].state
-    else
-      order_state = -1
+    var res6 = await mysql("orderinfo").where({ order_id })
+    if (res6.length > 0)
+      continue
 
     if (count == 0)
       str += "{"
     else
       str += ",{"
     str += "\"order_type\":\"早餐\","
-    str += "\"food_oder_id\":\"" + food_order_id + "\","
+    str += "\"food_order_id\":\"" + order_id + "\","
     str += "\"order_shop_address\":\"" + order_shop_address + "\","
     str += "\"order_deli_fee\": \"" + order_deli_fee + "\","
     str += "\"order_total_item\": \"" + order_total_item + "\","
@@ -82,12 +75,10 @@ module.exports = async ctx => {
     str += "\"order_address\": \"" + order_address + "\","
     str += "\"order_deli_time\": \"明早7: 00 - 8: 00送达\","
     str += "\"order_time\": \"" + order_time + "\","
-    str += "\"order_state\": \"" + order_state + "\","
     str += "\"order_sum\": \"" + order_sum + "\""
     str += "}"
-    count+=1
+    count += 1
   }
   str += "]}"
   ctx.state.data = JSON.parse(str)
-      
 }
